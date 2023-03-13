64c247212f653ae93998915f40768df0865856f3
altavada <117209509+altavada@users.noreply.github.com>
2023-03-13 19:06:09 -0400

    Created tag seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,12 @@
 const sequelize = require('../config/connection');
-const { User, Posts } = require('../models');
+const { User, Posts, Tags } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const tagData = require('./tagData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
@@ -17,6 +17,11 @@ const seedDatabase = async () => {
     returning: true,
   });
   console.log('--------------POSTS SEEDED---------------');
+  await Tags.bulkCreate(tagData, {
+    individualHooks: true,
+    returning: true,
+  });
+  console.log('--------------TAGS SEEDED---------------');
   process.exit(0);
 };
 

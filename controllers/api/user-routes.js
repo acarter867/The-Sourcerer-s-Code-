86556acc4e416e51f2c0868626c4d52ad94b2c3b
ACarter867 <acarter867@icloud.com>
2023-03-16 16:43:30 -0400

    Current session username shows up next to logout button

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,6 +14,7 @@ router.post("/", async (req, res) => {
     });
 
     req.session.save(() => {
+      req.session.username = userData.username;
       req.session.logged_in = true;
       console.log(req.session.logged_in);
       req.session.userId = userData.id;
@@ -51,6 +52,7 @@ router.post("/login", async (req, res) => {
     }
 
     req.session.save(() => {
+      req.session.username = userData.username;
       req.session.logged_in = true;
       req.session.userId = userData.id;
       res.status(200).json({ user: userData, message: "You are now logged in!" });
